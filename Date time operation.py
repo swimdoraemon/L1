13b161685d37701246d28115c560a468d08e574b
swimdoraemon <indoaradhya@gmail.com>
2024-09-05 19:01:06 +0900

    calender

---
+++ Date time operation.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+current_time=datetime.datetime.now()
+
+
+print("Time now at greenwich meridian is :", end ="")
+print(current_time)
+
+
+import calendar
+print("\n", calendar.calendar(2024))
